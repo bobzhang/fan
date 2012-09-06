e88f431db7225c1becf8fda2690e329824ec5e80
HongboZhang <bobzhang1988@gmail.com>
2012-09-05 21:18:21 -0400

    tweak

---
+++ src/DebugParser.ml
@@ -0,0 +1,86 @@
+module IdDebugParser = struct
+  value name = "Camlp4DebugParser";
+  value version = Sys.ocaml_version;
+end;
+
+module MakeDebugParser (Syntax : FanSig.Camlp4Syntax) = struct
+  open FanSig;
+  include Syntax;
+
+  module StringSet = Set.Make String;
+
+  value debug_mode =
+    try
+      let str = Sys.getenv "STATIC_CAMLP4_DEBUG" in
+      let rec loop acc i =
+        try
+          let pos = String.index_from str i ':' in
+          loop (StringSet.add (String.sub str i (pos - i)) acc) (pos + 1)
+        with
+        [ Not_found ->
+            StringSet.add (String.sub str i (String.length str - i)) acc ] in
+      let sections = loop StringSet.empty 0 in
+      if StringSet.mem "*" sections then fun _ -> True
+      else fun x -> StringSet.mem x sections
+    with [ Not_found -> fun _ -> False ];
+
+  value rec apply accu =
+    fun
+    [ [] -> accu
+    | [x :: xs] ->
+        let _loc = Ast.loc_of_expr x
+        in apply <:expr< $accu$ $x$ >> xs ];
+
+  value mk_debug_mode _loc = fun [ None -> <:expr< Debug.mode >>
+                                 | Some m -> <:expr< $uid:m$.Debug.mode >> ];
+
+  value mk_debug _loc m fmt section args =
+    let call = apply <:expr< Debug.printf $str:section$ $str:fmt$ >> args in
+      <:expr< if $mk_debug_mode _loc m$ $str:section$ then $call$ else () >>;
+
+  EXTEND Gram
+    GLOBAL: expr;
+    expr:
+    [ [ m = start_debug; section = LIDENT; fmt = STRING;
+        args = LIST0 expr LEVEL "."; x = end_or_in ->
+      match (x, debug_mode section) with
+      [ (None,   False) -> <:expr< () >>
+      | (Some e, False) -> e
+      | (None, _) -> mk_debug _loc m fmt section args
+      | (Some e, _) -> <:expr< let () = $mk_debug _loc m fmt section args$ in $e$ >> ]
+    ] ];
+    end_or_in:
+    [ [ "end" -> None
+      | "in"; e = expr -> Some e
+    ] ];
+    start_debug:
+    [ [ LIDENT "debug" -> None
+      | LIDENT "camlp4_debug" -> Some "Camlp4"
+    ] ];
+  END;
+
+end;
+
+
+
+(* load debug parser for bootstrapping *)  
+value pa_debug (module P:FanSig.PRECAST) =
+  P.ocaml_syntax_extension (module IdDebugParser) (module MakeDebugParser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
