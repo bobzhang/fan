ce3160bd39639d344f4ad78a00e5ed4514066c4a
bobzhang <hongboz@seas.upenn.edu>
2012-10-21 18:00:35 -0400

    improve printer:fix

--- test/ast_printer_test.ml
+++ test/ast_printer_test.ml
@@ -20,3 +20,75 @@ type u = ?v:int -> ?l:int -> m:int -> string
 
 type u = (a*b) -> ?v:int -> ?l:int -> m:int -> string
 
+
+type 'a u = [< `a | `b] as 'a 
+type 'a u = [< `a | `b > `a] as 'a
+type u = [`a | `b]
+
+type 'a ab = [< `a|`b] as 'a 
+type 'a ac = 'a constraint 'a = [< `a | `c ]
+type ('a,'b) m = [< `m of 'a ab & 'a ac ] as 'b
+
+type 'a u = [< `a | `b > `a `b]
+
+type 'a c =  < draw:int; .. >  as 'a
+
+class a = object end
+type 'a u = (#a as 'a)    
+
+class type a = object method v : int end
+
+type 'c u = ('a,'b)#a as 'c     
+
+module type S = sig 
+  class ['a, 'b] f : 'a -> 'b -> object method x : 'a method y : 'b end
+end
+module type S = sig
+  class ['a, 'b] f : f:'a -> ?g:'b -> object method x : 'a method y : 'b end
+end;;      
+
+class ['a,'b] f (v:'a) (u:'b) = object method x = v method y = u end
+
+type ('a,'b,'c)u = ('a,'b) #f as 'c    
+
+let f ~v:v0 ~u = u + v0;;
+
+let f ~v:(v0:int) y = v0 + y;;
+
+class ['a,'b] f ~v:(v:'a) ~u:(u:'b) = object method x = v method y = u end;;
+(* class ['a, 'b] f : v:'a -> u:'b -> object method x : 'a method y : 'b end;; *)
+
+type ('a,'b,'c) u  = ('a,'b)#f [> `c `a] as 'c;;
+(* empty should not print*)
+ 
+class ['a] circle (c : 'a) = object
+  constraint 'a = #point
+  val mutable center = c
+  method center = center
+  method set_center c = center <- c
+  method move = center#move
+end
+
+let sum (lst : _ #iterator) = lst#fold (fun x y -> x+y) 0    
+    
+let f y ?(x=3) z = x + y + z 
+let f ~y:y0 = 3
+let f ?x:x0 y = x+y0
+
+let f ?x:(Some x0) y = x0+y;;
+
+let f ?x:x y = match x with Some x -> x +y0;;
+
+let f ?x y = match x with Some x -> x +y;;
+
+let f x = function
+  | Some y -> 1
+  | None -> 0 
+
+let a f = f
+and g f =
+  g ; g
+
+let f ()  =
+  let g = a
+  and g =  3 in g ;g
