4dfcccc6aad013af577380cbb0455aa8d0dd4fb5
bobzhang <hongboz@seas.upenn.edu>
2012-11-01 22:46:10 -0400

    clean:fix

--- test_r/true_false.ml
+++ test_r/true_false.ml
@@ -1,26 +1,26 @@
 
 
-(* let _ = (true,false); *)
-false;
-(* let a = fun *)
-(*   [ true -> true *)
-(*   | false -> false]; *)
-(* let b = fun *)
-(*   [ `true -> true *)
-(*   | `false -> false]; *)
+let _ = (true,false);
+(* false; *)
+let a = fun
+  [ true -> true
+  | false -> false];
+let b = fun
+  [ `true -> true
+  | `false -> false];
 
 
-(* let a = (`true,`false,`True,`False); *)
+let a = (`true,`false,`True,`False);
 
-(* exception True of bool ; *)
+exception True of bool ;
 
-(* type a = [ True of int ]; *)
+type a = [ True of int ];
 
-(* let a = fun *)
-(*   [ true ->  true *)
-(*   | false as x -> x]; *)
-(* let is_constructor =  fun *)
-(*   [ true -> assert false ]; *)
+let a = fun
+  [ true ->  true
+  | false as x -> x];
+let is_constructor =  fun
+  [ true -> assert false ];
 
 let _ = assert false;  
 (* assert false; *)
