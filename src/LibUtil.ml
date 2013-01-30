fb76d912d535e45a4d4fb66d90c7f6b3350baebe
HongboZhang <bobzhang1988@gmail.com>
2013-01-30 11:11:13 -0500

    enhance queue in LibUtil

--- src/LibUtil.ml
+++ src/LibUtil.ml
@@ -132,7 +132,21 @@ module Queue = struct
     with_return (fun r ->
       (iter (fun x -> match f x with [None -> () | Some _ as res -> r.return res]) t;
       None));
+  (* the first element is in the bottom *)  
+  let to_list_rev q =
+    fold (fun acc v -> [v::acc]) [] q ;
 
+  let of_list l =
+    let q = create () in
+    let _ = List.iter (fun x -> push x q) l in 
+    q;
+    
+  let rev q=
+    of_list (to_list_rev q );
+    (* let nq = create () in *)
+    (* while not (empty q) do *)
+    (*   push pop q *)
+    (* done *)
     
 end;
 module List = struct
