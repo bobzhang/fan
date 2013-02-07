0de7ba90df94103bcb162f477f390da0b9346f0b
bobzhang <hongboz@seas.upenn.edu>
2013-02-07 00:04:26 -0500

    unify the types

--- src/Ant.mli
+++ src/Ant.mli
@@ -1,9 +1,5 @@
 open Ast
-module MetaLocQuotation :
-  sig
-    val meta_loc_expr : loc -> loc -> expr
-    val meta_loc_patt : loc -> 'a -> patt
-  end
+module MetaLocQuotation : FanAst.META_LOC
 
 val antiquot_expander : parse_patt:(loc -> string -> patt) ->
   parse_expr:(loc -> string -> expr) ->  FanAst.map
