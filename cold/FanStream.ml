f42c3c0f14b75a709a799af6435a04de6a04fad3
HongboZhang <bobzhang1988@gmail.com>
2013-02-18 16:24:25 -0500

    clean

--- cold/FanStream.ml
+++
@@ -1,7 +0,0 @@
-open Ast
-type 'a item_or_def =  
-  | SdStr of 'a
-  | SdDef of string* (string list* expr) option
-  | SdUnd of string
-  | SdITE of bool* 'a item_or_def list* 'a item_or_def list
-  | SdLazy of 'a Lazy.t 
--- src/FanStream.ml
+++
@@ -1,7 +0,0 @@
-open Ast;
-type item_or_def 'a =
-  [ SdStr of 'a
-  | SdDef of string and option (list string * expr)
-  | SdUnd of string
-  | SdITE of bool and list (item_or_def 'a) and list (item_or_def 'a)
-  | SdLazy of Lazy.t 'a ];
