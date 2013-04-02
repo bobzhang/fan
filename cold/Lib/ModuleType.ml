d0d6433fa023464e28d5ce15eb6a069df6c6de80
HongboZhang <bobzhang1988@gmail.com>
2013-04-02 00:08:27 -0400

    clean

--- cold/Lib/ModuleType.ml
+++
@@ -1,8 +0,0 @@
-let app mt1 mt2 =
-  match (mt1, mt2) with
-  | (`Id (_loc,i1),`Id (_,i2)) -> `Id (_loc, (`App (_loc, i1, i2)))
-  | _ -> invalid_arg "Fan_module_type app"
-let acc mt1 mt2 =
-  match (mt1, mt2) with
-  | (`Id (_loc,i1),`Id (_,i2)) -> `Id (_loc, (`Dot (_loc, i1, i2)))
-  | _ -> invalid_arg "ModuleType.acc"
--- src/Lib/ClassExpr.ml
+++
@@ -1,9 +0,0 @@
-
-(* open Ast; *)
-#default_quotation "class_exp";;
-(* INCLUDE "src/Lib/CommonStructure.ml"; *)
-
-(* let rec view_app al =  fun *)
-(*   [ {| $ce $a |} ->view_app [a :: al] ce *)
-(*   | ce -> (ce, al) ]; *)
-
