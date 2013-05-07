467c879d67ff1f40f2241897c29d30d2daab122f
HongboZhang <bobzhang1988@gmail.com>
2013-05-07 18:22:13 -0400

    clean

--- cold/FanEval.ml
+++
@@ -1,8 +0,0 @@
-let s2s s = (Ast2pt.phrase s : Parsetree.toplevel_phrase )
-
-let eval_ast fmt ast =
-  let _snap = Btype.snapshot () in
-  try
-    Env.reset_cache_toplevel ();
-    ignore (Toploop.execute_phrase true fmt (s2s ast))
-  with | x -> (Errors.report_error fmt x; exit 2)
--- cold/ModuleType.ml
+++
@@ -1,9 +0,0 @@
-let app mt1 mt2 =
-  match (mt1, mt2) with
-  | ((i1 : Ast.mtyp),(i2 : Ast.mtyp)) -> (`Apply (_loc, i1, i2) : Ast.mtyp )
-  | _ -> invalid_arg "Fan_mtyp app"
-
-let acc mt1 mt2 =
-  match (mt1, mt2) with
-  | ((i1 : Ast.mtyp),(i2 : Ast.mtyp)) -> (`Dot (_loc, i1, i2) : Ast.mtyp )
-  | _ -> invalid_arg "ModuleType.acc"
