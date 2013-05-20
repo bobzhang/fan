29e37f700f5b52179b1829230ce671aac65f8332
HongboZhang <bobzhang1988@gmail.com>
2013-05-20 10:59:16 -0400

    preparing for merging

--- src/Ant.mli
+++
@@ -1,7 +0,0 @@
-open Ast
-
-(* module LocExpr: FanAst.META_LOC *)
-(* module LocPatt: FanAst.META_LOC     *)
-
-val antiquot_expander : parse_pat:(loc -> string -> pat) ->
-  parse_exp:(loc -> string -> exp) ->  Objs.map
--- src/AntN.mli
+++
@@ -1,5 +0,0 @@
-open Ast
-  
-val antiquot_expander :
-  parse_pat:(loc -> string -> pat) ->
-  parse_exp:(loc -> string -> exp) -> Objs.map
