dbdb7363a34e975e6def560e7524d64f5355bbd4
HongboZhang <bobzhang1988@gmail.com>
2013-05-20 13:13:41 -0400

    mac clean

---
+++ back/eP.mli
@@ -0,0 +1,7 @@
+(* val tuple_of_number : Ast.ep -> int -> Ast.ep *)
+(* val of_vstr_number : string -> int -> Ast.ep *)
+(* val gen_tuple_n : *)
+(*   ?cons_transform:(string -> string) -> arity:int -> string -> int -> Ast.ep *)
+(* val mk_record : ?arity:int -> FSig.col list -> Ast.ep *)
+(* val mk_tuple : arity:int -> number:int -> Ast.ep *)
+(* val of_str: string -> Ast.ep *)
