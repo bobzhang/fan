552282b8b2790f20693ba1ceeec2dd8578bbc00c
bobzhang1988@gmail.com <hongboz@seas.upenn.edu>
2013-05-20 12:56:25 -0400

    clean

--- src/Ant.mli
+++
@@ -1,9 +0,0 @@
-open Ast
-
-
-
-(*************************************************************************)    
-(* when the antiquotation appears in the pattern position,
-   its final context is [pat] *)  
-val antiquot_expander : parse_pat:(loc -> string -> pat) ->
-  parse_exp:(loc -> string -> exp) ->  Objs.map
