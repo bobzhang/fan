9df4a73e1d2e00bd6bcf7353788cbe3710a26070
bobzhang1988@gmail.com <hongboz@seas.upenn.edu>
2013-05-20 13:10:17 -0400

    clean all capitalized files

--- low.py
+++ low.py
@@ -1,5 +1,7 @@
+import glob 
 import os.path
-files=glob.glob('src/*.ml') + glob.glob('src/*.mli') + glob.glob('cold/*.mli') + glob.glob('cold/*.ml')
+files=glob.glob('src/*.ml') + glob.glob('src/*.mli')
+# + glob.glob('cold/*.mli') + glob.glob('cold/*.ml')
 
 def translate(s):
     return s[0].lower() +s[1:]
@@ -7,5 +9,8 @@ def translate(s):
 
 for i in files:
     base,name=os.path.split(i)
-    dst= os.path.join(base,translate(name))
-    os.rename(i,dst)
+    # dst= os.path.join(base,translate(name))
+    if name[0].isupper():
+        print name
+
+    # os.rename(i,dst)
