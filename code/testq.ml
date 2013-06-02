8a85740921eda018cf42537841c055716146e385
HongboZhang <bobzhang1988@gmail.com>
2013-06-01 21:38:45 -0400

    change links back to master and refine some examples

--- code/testq.ml
+++ code/testq.ml
@@ -3,9 +3,11 @@
 
 {:extend|
   p:
-  [pat{p};"when"; exp{e} -> {:exp| function | $pat:p when $e -> true |_ -> false |}
-  |pat{p} -> {:exp| function | $pat:p -> true | _ -> false |} ]
+  [pat{p};"when"; exp{e} -> {:exp| function | $p when $e -> true |_ -> false |}
+  |pat{p} -> {:exp| function | $p -> true | _ -> false |} ]
 |};;
+
+
 
 let d = `Absolute["Tutorial"];;
 AstQuotation.of_exp ~name:(d,"q") ~entry:p;;
