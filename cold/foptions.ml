605a4b54857b0f60a44c805f3cfd51e6e8d3132b
HongboZhang <bobzhang1988@gmail.com>
2013-08-11 18:01:48 -0400

    snapshot and clean

--- cold/foptions.ml
+++
@@ -1,6 +0,0 @@
-type spec_list = (string* Arg.spec* string) list 
-let init_spec_list = ref []
-let init spec_list = init_spec_list := spec_list
-let add (name,spec,descr) =
-  init_spec_list := (init_spec_list.contents @ [(name, spec, descr)])
-let adds ls = init_spec_list := (init_spec_list.contents @ ls)
