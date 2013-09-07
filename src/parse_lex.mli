a541fc679bd354cfb0fece0034c7e1e59479df18
HongboZhang <bobzhang1988@gmail.com>
2013-09-07 16:30:11 -0400

    tweak

--- src/parse_lex.mli
+++ src/parse_lex.mli
@@ -2,13 +2,7 @@ open FAst
 open Translate_lex
 
 
-(* val regexp_for_string : string -> concrete_regexp *)
-
 val named_regexps : (string, concrete_regexp) LibUtil.Hashtbl.t
-
-(* val remove_as : concrete_regexp ->concrete_regexp *)
-
-(* val as_cset : concrete_regexp -> Fcset.t *)
 
 val regexp : concrete_regexp Fgram.t
 
