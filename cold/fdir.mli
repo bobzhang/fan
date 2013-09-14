8b611393e04c1ad00fccfddbc6149c726bb0eec7
bobzhang <hongboz@seas.upenn.edu>
2013-09-14 09:18:35 -0400

    remove unused files

--- cold/fdir.mli
+++
@@ -1,9 +0,0 @@
-
-
-val dir_table : (string, FLoc.t -> string -> unit) Hashtbl.t
-
-(* val handle : FLoc.t -> FToken.quotation -> unit *)
-
-val register : string * (FLoc.t -> string -> unit) -> unit
-
-val handle_dir : FLoc.t -> string * string -> unit    
