7cde1b783f924d98b8bccd8361bfc99be74e70fb
HongboZhang <bobzhang1988@gmail.com>
2013-09-26 15:24:10 -0400

    alplha works ^^

--- clean.sh
+++ clean.sh
@@ -1,1 +1,3 @@
+set -e 
 rm -rf *.omc common/*.cmo common/*.cmi common/*.cma common/*.omc  treeparser/*.cmo treeparser/*.cmi treeparser/*.cma treeparser/*.omc cold/*.cmo cold/*.cmi cold/*.cma  cold/*.omc
+rm -rf fan fan.run fan.opt
