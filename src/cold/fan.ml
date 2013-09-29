fdb27800acff12923fa32b69bc4b7f8ddbc33ad8
bobzhang <hongboz@seas.upenn.edu>
2013-09-29 01:44:36 -0400

    snapshot workflow seems to work

--- src/cold/fan.ml
+++ src/cold/fan.ml
@@ -13,6 +13,6 @@ let _ =
   Ast_parsers.use_parsers ["revise"; "stream"];
   (try
      Arg.parse_dynamic Foptions.init_spec_list MkFan.anon_fun
-       "fan <options> <file>\nOptions are:\n"
+       "fan <options> <file>\nOptions are:"
    with
    | exc -> (Format.eprintf "@[<v0>%s@]@." (Printexc.to_string exc); exit 2))
