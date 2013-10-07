4c88c93deb467d422fb9e4ba868f804026f4f512
HongboZhang <bobzhang1988@gmail.com>
2013-10-06 21:32:13 -0400

    split ast_loc

---
+++ src/main/ast_loc.ml
@@ -0,0 +1,10 @@
+(** generate [loc_of] function per type
+    for example:
+    [loc_of (e:exp)]
+    [loc_of (p:pat)] will both return the location information *)
+{:fans|keep off; derive (GenLoc);|};;
+{:ocaml|{:include|"../common/fAst.mli"|}|};;
+
+(* local variables: *)
+(* compile-command: "cd .. && pmake main_annot/ast_loc.cmo" *)
+(* end: *)
