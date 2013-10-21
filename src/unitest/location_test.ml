208369f4b1248857ed7467ee6a22dfb5b49d84b9
HongboZhang <bobzhang1988@gmail.com>
2013-10-21 11:11:15 -0400

    upgrade unitest

---
+++ src/unitest/location_test.ml
@@ -0,0 +1,6 @@
+type t = (int, float) Unbound.t
+      
+
+(* local variables: *)
+(* compile-command: "cd .. && pmake unitest_annot/location_test.cmo" *)
+(* end: *)
--- src/unitest/test_grammar.ml
+++ src/unitest/test_grammar.ml
@@ -7,31 +7,47 @@ open Test_util
 let () =
   Ast_parsers.use_parsers ["revise"]
 
-let t_meta  = Fgram.parse_string Parse_grammar.simple_meta     
+let t_meta  = Fgram.parse_string Parse_grammar.simple
 
 let test_simple_meta _ =
-  let u = t_meta "`Lid _" in
+  let u = t_meta "Lid _" in
   match u with
-  | {text = `Stok (_,
+  | [{text = `Stok (_,
                    %exp@_{function | `Lid _ -> true | _ -> false},
-                   %exp@_{%pat-'{`Lid _}} ,
+                   %exp@_{ ("Lid", `Any)} ,
                    _);
      pattern= Some %pat@_{`Lid _};
-     _} ->  ()
+     _}] ->  ()
   | _ -> assert_failure "test_simple not token"
 
+(* (%p{[{Gram_def.text = `Stok (_, *)
+(*                    %exp@_{function | `Lid _ -> true | _ -> false}, *)
+(*                    %exp@_{%pat-'{`Lid _}} ,_); _}]} u) *)        
 let test_simple_meta1 _ =
-  let u = t_meta "`Lid x" in
+  let u = t_meta "Lid x" in
   match u with
-  | {text = `Stok (_,
+  | [{text = `Stok (_,
                    %exp@_{function | `Lid _ -> true | _ -> false},
-                   %exp@_{%pat-'{`Lid _}} ,_);
+                   %exp@_{("Lid",`Any)} ,_);
      pattern = Some %pat@_{`Lid x } ;
-     _ } ->  ()
+     _ }] ->  ()
 
   | _ -> assert_failure "test_simple not token"
   
-
+let test_simple_meta2 _ =
+  let u = t_meta %str{("ghso"|"a" as x)} in
+  match u with
+  | [{text =  `Skeyword(_,"ghos");
+     styp = `Tok _ ; (* Wrong?*)
+     pattern =
+     Some %pat@_{`Key x};
+   };
+     {text =  `Skeyword(_,"ghos");
+     styp = `Tok _ ; (* Wrong?*)
+     pattern =
+     Some %pat@_{`Key x};
+   }] -> ()
+  | _ -> assert_failure "test_simple_meta2 " 
     
 let suite =
   "Test_grammar" >:::
