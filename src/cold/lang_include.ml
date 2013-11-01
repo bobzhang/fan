81046df873fbad20b97de14d2bcb09dea78bf88c
Hongbo Zhang <bobzhang1988@gmail.com>
2013-11-01 00:33:44 -0400

    tweak

--- src/cold/lang_include.ml
+++ src/cold/lang_include.ml
@@ -7,15 +7,14 @@ let _ =
          [([`Token
               (((function | `Str _ -> true | _ -> false)), (4153489, `Any),
                 "`Str s")],
-            ("let (keep,cf) = let open State in (keep, current_filters) in\nlet fan_keep__0 = keep.contents and fan_cf__1 = cf.contents in\ntry\n  let fan_res__2 = State.reset (); Gramf.parse_include_file Syntaxf.strus s in\n  let _ = keep := fan_keep__0; cf := fan_cf__1 in fan_res__2\nwith | fan_e__3 -> ((keep := fan_keep__0; cf := fan_cf__1); raise fan_e__3)\n",
+            ("let (keep,cf) = let open State in (keep, current_filters) in\nlet fan_keep__0 = !keep and fan_cf__1 = !cf in\ntry\n  let fan_res__2 = State.reset (); Gramf.parse_include_file Syntaxf.strus s in\n  let _ = keep := fan_keep__0; cf := fan_cf__1 in fan_res__2\nwith | fan_e__3 -> ((keep := fan_keep__0; cf := fan_cf__1); raise fan_e__3)\n",
               (Gramf.mk_action
                  (fun ~__fan_0:(__fan_0 : Tokenf.t)  (_loc : Locf.t)  ->
                     match __fan_0 with
                     | `Str ({ txt = s;_} : Tokenf.txt) ->
                         (let (keep,cf) =
                            let open State in (keep, current_filters) in
-                         let fan_keep__0 = keep.contents
-                         and fan_cf__1 = cf.contents in
+                         let fan_keep__0 = !keep and fan_cf__1 = !cf in
                          (try
                             let fan_res__2 =
                               State.reset ();
