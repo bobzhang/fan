cadd0348292bdcfaa0f15db05aa2ea42b616f3e5
Hongbo Zhang <bobzhang1988@gmail.com>
2013-12-08 16:23:03 -0500

    move ast_basic to main

--- src/main/ast_basic.ml
+++ src/main/ast_basic.ml
@@ -6,22 +6,20 @@
 
 
 (** connecting a list of nodes from right to the left
-  It assumes that the associativity is right.
+  It assumes that the associativity is *right*.
 
   For example,
   {[
    com_of_list [ `Int(_loc,2 ); `Int (_loc,3); `Int (_loc,4) ]
-   `Com (, `Int (, 2), `Com (, `Int (, 3), `Int (, 4)))
-   ]}
+   `Com (, `Int (, 2), `Com (, `Int (, 3), `Int (, 4)))]}
   
-  Note that [com_of_list] is making use of [of_listr]
-
-  *)
+  Note that [com_of_list] and other utilities  is making use of [of_listr] *)
 let rec of_listr f xs =
   match xs with
-  | [] -> invalid_arg "of_listr empty"
+  | [] -> invalid_arg __BIND__
   | [t] -> t
   | t :: ts -> f t (of_listr f ts)
+
 
 (**
    connecting a list of nodes from left to right.
@@ -33,7 +31,7 @@ let rec of_listr f xs =
  *)
 let rec of_listl f xs =
   match xs with
-  | [] -> invalid_arg "of_listl empty"
+  | [] -> invalid_arg __BIND__
   | [t] -> t
   | x::y::xs -> of_listl f (f x y :: xs)
 
---
+++ src/main/ast_basic.mli
@@ -0,0 +1,56 @@
+
+
+val of_listr : ('a -> 'a -> 'a) -> 'a list -> 'a
+val of_listl : ('a -> 'a -> 'a) -> 'a list -> 'a
+
+val list_of :
+  ([< `And of 'b * 'a * 'a
+    | `App of 'c * 'a * 'a
+    | `Bar of 'd * 'a * 'a
+    | `Com of 'e * 'a * 'a
+    | `Dot of 'f * 'a * 'a
+    | `Sem of 'g * 'a * 'a
+    | `Sta of 'h * 'a * 'a ]
+   as 'a) ->
+  'i -> 'i
+
+val list_of :
+    ([> `And of 'b * 'a * 'a
+     | `App of 'c * 'a * 'a
+     | `Bar of 'd * 'a * 'a
+     | `Com of 'e * 'a * 'a
+     | `Dot of 'f * 'a * 'a
+     | `Sem of 'g * 'a * 'a
+     | `Sta of 'h * 'a * 'a ]
+       as 'a) ->
+         'a list -> 'a list
+      
+val list_of_and : ([> `And of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val fold_and_right :
+    (([> `And of 'b * 'a * 'a ] as 'a) -> 'c -> 'c) -> 'a -> 'c -> 'c
+        
+val list_of_com : ([> `Com of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val list_of_star : ([> `Sta of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val list_of_bar : ([> `Bar of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val list_of_sem : ([> `Sem of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val list_of_dot : ([> `Dot of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val list_of_app : ([> `App of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val listr_of_arrow :
+  ([> `Arrow of 'b * 'a * 'a ] as 'a) -> 'a list -> 'a list
+val view_app : 'a list -> ([> `App of 'c * 'b * 'a ] as 'b) -> 'b * 'a list
+
+
+module N : sig
+  val list_of_and : ([> `And of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_com : ([> `Com of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_star : ([> `Sta of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_bar : ([> `Bar of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_or : ([> `Bar of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_sem : ([> `Sem of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_dot : ([> `Dot of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val list_of_app : ([> `App of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val listr_of_arrow :
+      ([> `Arrow of 'a * 'a ] as 'a) -> 'a list -> 'a list
+  val view_app : 'a list -> ([> `App of 'b * 'a ] as 'b) -> 'b * 'a list          
+end
+    
