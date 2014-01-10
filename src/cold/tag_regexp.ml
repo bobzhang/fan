14095394a4340a52b4f8a7c88d27298647120b58
Hongbo Zhang <bobzhang1988@gmail.com>
2014-01-10 11:00:30 -0500

    tweak

--- src/cold/tag_regexp.ml
+++ src/cold/tag_regexp.ml
@@ -1,13 +1,12 @@
-type tag_info = {id : string ; start : bool ; action : int}
-
-type regexp =
-  
-
-  | Chars of int * bool
+type tag_info =  {
+  id: string;
+  start: bool;
+  action: int} 
+type regexp =  
+  | Chars of int* bool
   | Action of int
   | Tag of tag_info
-
   | Empty
-  | Seq of regexp * regexp
-  | Alt of regexp * regexp
-  | Star of regexp
+  | Seq of regexp* regexp
+  | Alt of regexp* regexp
+  | Star of regexp 
