16fe9577f7f6a39785f44c6be19c38acf1da2fc9
Hongbo Zhang <bobzhang1988@gmail.com>
2015-06-19 19:02:35 -0400

    use symlink for fan, fan_top

--- src/cold/fan.ml
+++ src/cold/fan.ml
@@ -1,8 +1,1 @@
-let _ =
-  Options.adds Fan_args.initial_spec_list;
-  Ast_parsers.use_parsers ["fan"];
-  (try
-     Arg.parse_dynamic Options.init_spec_list Fan_args.anon_fun
-       "fan <options> <file>\nOptions are:"
-   with
-   | exc -> (Format.eprintf "@[<v0>%s@]@." (Printexc.to_string exc); exit 2))
+../main/fan.ml
