7e8b83eb30bd5a09e8598c82f580492895848f7d
Hongbo Zhang <bobzhang1988@gmail.com>
2015-06-20 09:48:44 -0400

    move dump.ml and dyn_load.ml to symlink

--- src/main/dyn_load.mli
+++ src/main/dyn_load.mli
@@ -1,14 +1,1 @@
-
-
-(** a simple encapsulation for dynamic loading in Fan *)    
-
-
-
-(** [load f] Load the file [f]. If [f] is not an absolute path name,
-    the load path list used to find the directory of [f]. It searches
-    in [Configf.dynload_dirs] *)
-val load : string -> unit
-    
-val objext : string
-
-val libext : string    
+../cold/dyn_load.mli
