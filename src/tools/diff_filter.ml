aa0394f5306119d2ba5465f9b3418ec38a740df5
Hongbo Zhang <bobzhang1988@gmail.com>
2015-06-23 20:54:07 -0400

    add filter files

---
+++ src/tools/diff_filter.ml
@@ -0,0 +1,26 @@
+let rec filter fmt = %lex{
+| '[' ['0'-'9']+ ',' ['0'-'9']+ '+' ['0'-'9']+ ']'   %{
+  begin 
+    Format.pp_print_string fmt "[]"; filter fmt lexbuf
+  end
+}
+| _ as c %{ 
+  begin 
+    Format.pp_print_char fmt c; filter fmt lexbuf
+  end
+  }
+| eof %{()}
+}
+
+
+(* let () =  *)
+(*   (filter Format.std_formatter @@ Lexing.from_string "(xx.ml[1,0+0]..[1,0+12])") *)
+
+let () = 
+  let in_chan = stdin in
+  let lex_buf = Lexing.from_channel in_chan in
+  let () = filter Format.std_formatter lex_buf in
+  close_in in_chan
+(* local variables: *)
+(* compile-command: "ocamlbuild  -cflags '-w -40'  -pp fan  diff_filter.byte " *)
+(* end: *)
